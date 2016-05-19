e4145458cc862fd1fb7863e6e614c8a4c5ddc442
root <root@localhost.localdomain>
2016-05-19 14:52:21 +0800

    record nodes in ./.fast.py.config
    Signed-off-by: root <root@localhost.localdomain>

--- python_script/fast.py
+++ python_script/fast.py
@@ -26,15 +26,33 @@ def multi_thread(func, nodes, cmd):
 
 def despatch_cmd(args):
         cmd = args[-1]
+        config_file = './.fast.py.config'
+        if os.path.isfile(config_file) == False:
+                os.mknod(config_file)
         print cmd
         nodes = args[1:-1]
+        if len(nodes) == 0:
+                fp = open (config_file, 'rb')
+                fnodes = fp.readline()
+                nodes = fnodes.split(' ') 
+        else:
+                fp = open (config_file, 'wb')
+                fp.truncate(0)
+                for i in range(0, len(nodes)):
+                        if i == len(nodes)-1:
+                                fp.write(nodes[i])
+                        else:
+                                fp.write(nodes[i] + ' ')
         print nodes
         multi_thread(__despatch_cmd, nodes, cmd)
 
 if __name__ == '__main__':
         if len(sys.argv) < 2:
-                print "python " + __file__ + " your_nodes" + " your_commands\n" \
-                      + "e.g. python " + __file__ + " node-1 node-2 node-3 \"date\"\n"
+                print "Usage: python " + __file__ + " your_nodes" + " your_commands\n" \
+                      + "e.g. python " + __file__ + " 10.10.21.9{1,2,3} \"date\""
+                print 'Notes: When python ' + __file__ + ' 10.10.21.9{1,2,3} date,\n'\
+                      + 'it can record your_nodes in the .fast.py.config,\n'\
+                      + 'and next time you can directly execute ./fast.py \"date\"'
                 exit()
         
         despatch_cmd(sys.argv)
