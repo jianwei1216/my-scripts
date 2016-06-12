69397fd9afb4d230da457b2bcf102dca4825dab2
zhangjianwei <zhangjianwei@estor.com.cn>
2016-06-13 02:10:00 +0800

    add copy.sh
    Signed-off-by: zhangjianwei <zhangjianwei@estor.com.cn>

---
+++ bash_scripts/copy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+copy_dir="/var/lib/digioceand/ /var/log/digioceanfs/ /var/run/digiocean/"
+save_dir=""
+ip_pre="10.10.21.11"
+ip_save="10.10.12.16"
+
+function mycopy()
+{
+    for i in 1 2 3; do
+        tmp_dir1="digioceanfs.${ip_pre}${i}"
+        tmp_dir2="/var/${tmp_dir1}"
+        echo ssh ${ip_pre}${i} "mkdir -p $tmp_dir2; cp -rf ${copy_dir} $tmp_dir2; cd /var/; tar -czvf ${tmp_dir1}.tar.gz $tmp_dir1; scp ${tmp_dir1}.tar.gz ${save_dir}; rm -rf ${tmp_dir1}.tar.gz ${tmp_dir1}" 
+        ssh ${ip_pre}${i} "mkdir -p $tmp_dir2; cp -rf ${copy_dir} $tmp_dir2; cd /var/; tar -czvf ${tmp_dir1}.tar.gz $tmp_dir1; scp ${tmp_dir1}.tar.gz ${save_dir}; rm -rf ${tmp_dir1}.tar.gz ${tmp_dir1}" 
+        echo
+    done
+}
+case $1 in
+  --save-dir)
+      save_dir=${ip_save}:$2
+      mycopy
+      ;;
+  *)
+      echo "Usage: $0 --save-dir <DIR>"
+      ;;
+esac
