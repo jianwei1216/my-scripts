8cf8f78231706ec988f5f6d575e0786c1b7fa2ae
zhangjianwei <zhangjianwei@estor.com.cn>
2016-06-13 10:28:27 +0800

    fix copy.sh no ps aux
    Signed-off-by: zhangjianwei <zhangjianwei@estor.com.cn>

--- bash_scripts/copy.sh
+++ bash_scripts/copy.sh
@@ -11,7 +11,7 @@ function mycopy()
         tmp_dir1="digioceanfs.${ip_pre}${i}"
         tmp_dir2="/var/${tmp_dir1}"
         echo ssh ${ip_pre}${i} "mkdir -p $tmp_dir2; cp -rf ${copy_dir} $tmp_dir2; cd /var/; tar -czvf ${tmp_dir1}.tar.gz $tmp_dir1; scp ${tmp_dir1}.tar.gz ${save_dir}; rm -rf ${tmp_dir1}.tar.gz ${tmp_dir1}" 
-        ssh ${ip_pre}${i} "mkdir -p $tmp_dir2; cp -rf ${copy_dir} $tmp_dir2; cd /var/; tar -czvf ${tmp_dir1}.tar.gz $tmp_dir1; scp ${tmp_dir1}.tar.gz ${save_dir}; rm -rf ${tmp_dir1}.tar.gz ${tmp_dir1}" 
+        ssh ${ip_pre}${i} "mkdir -p $tmp_dir2; ps aux|grep digiocean|grep -v grep > ${tmp_dir2}/digioceanfs.ps.aux; cp -rf ${copy_dir} $tmp_dir2; cd /var/; tar -czvf ${tmp_dir1}.tar.gz $tmp_dir1; scp ${tmp_dir1}.tar.gz ${save_dir}; rm -rf ${tmp_dir1}.tar.gz ${tmp_dir1}" 
         echo
     done
 }
