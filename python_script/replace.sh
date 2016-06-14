348cd14955a038dbe9a38a85ca79a5aef39846ee
root <root@node-1.test>
2016-06-14 12:56:13 +0800

    fix digioceanfs_client
    Signed-off-by: root <root@node-1.test>

--- python_script/replace.sh
+++ python_script/replace.sh
@@ -6,7 +6,7 @@ client="/usr/local/digioceanfs_client/"
 
 \cp -r /root/git/MGMT/digioceanfs_manager/core/* ${manager}
 \cp -r /root/git/MGMT/digioceanfs_gui/core/* ${gui}
-\cp -r /root/git/MGMT/digioceanfs_client/core/* ${client}
+\cp -r /root/git/MGMT/digioceanfs_client/* ${client}
 
 python /root/git/MGMT/mkpyc.py ${manager}
 python /root/git/MGMT/mkpyc.py ${gui}
