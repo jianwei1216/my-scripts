a09063fa9893d1aa170650cbb226c94f634a0990
zhangjianwei <zhangjianwei@estor.com.cn>
2016-08-26 15:40:43 +0800

    添加快速ln -s脚本

---
+++ bash_scripts/ln_s.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+ln -s /root/git/my-scripts/bash_scripts/multi_nodes_fast.sh /usr/local/bin/multi_nodes_fast
+ln -s /root/git/my-scripts/python_script/fast.py /usr/local/bin/fast
+ln -s /root/git/my-scripts/python_script/misc.py /usr/local/bin/misc
